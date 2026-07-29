5a64b62f0d08a41e97b400a05263dcd68634cfc0
Any5123 <amdielywork@gmail.com>
2026-07-29 14:41:30 -0500

    Update calendario.js

--- js/calendario.js
+++ js/calendario.js
@@ -17,7 +17,7 @@ const avatarEl = document.getElementById('avatar');
 const volverEl = document.getElementById('volver');
 const msgArea = document.getElementById('msgArea');
 
-const roles = { docente: 'Docente', psicologo: 'Psicólogo institucional', coordinador: 'Coordinador' };
+const roles = { docente: 'Docente', psicologo: 'Psicóloga institucional', coordinador: 'Coordinador', rector: 'Rector' };
 
 volverEl.href = categoria === 'coordinador' ? 'lista.html?categoria=coordinador' : `lista.html?categoria=${categoria}`;
 
